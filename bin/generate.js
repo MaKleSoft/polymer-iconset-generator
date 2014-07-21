dd3cfe36feafcc22a176eacca4e315946fec4e97
Martin Kleinschrodt <martin@maklesoft.com>
2014-07-21 13:27:23 +0200

    Initial commit

---
+++ bin/generate.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var path = require("path"),
+    generate = require("../index.js"),
+    argv = require("yargs").argv,
+    srcDir = argv._[0] && path.normalize(argv._[0]) || "./";
+
+generate(srcDir, {
+    dest: argv.dest && path.normalize(argv.dest),
+    lib: argv.lib && path.normalize(argv.lib),
+    name: argv.name,
+    size: argv.size
+});
---
+++ index.js
@@ -0,0 +1,48 @@
+var fs = require("fs"),
+    path = require("path"),
+    svgPattern = /<svg[\w\W]*?>([\w\W]*)<\/svg>/;
+
+function read(file) {
+    var name = path.basename(file, ".svg"),
+        content = fs.readFileSync(file, {encoding: "utf-8"}),
+        match = content.match(svgPattern);
+
+    if (!match) {
+        console.log("File " + file + " did not contain a valid svg element!");
+        return "";
+    }
+
+    return "\n<g id=\"" + name + "\">\n" + match[1] + "\n</g>";
+}
+
+function readDir(dir) {
+    var defs = [];
+    fs.readdirSync(dir).forEach(function(entry) {
+        var fullPath = path.join(dir, entry);
+        if (path.extname(entry) == ".svg") {
+            defs.push(read(fullPath));
+        }
+    });
+    return defs;
+}
+
+function generate(srcDir, opts) {
+    opts = opts || {};
+    var setName = opts.name || path.basename(srcDir),
+        dest = opts.dest || setName + ".html",
+        libPath = opts.lib || "core-iconset-svg.html",
+        relLibPath = path.extname(dest) == ".html" ?
+            path.relative(path.dirname(dest), libPath) : path.relative(dest, libPath),
+        size = opts.size || "100",
+        libImport = "<link rel=\"import\" href=\"" + relLibPath + "\">",
+        defs, html;
+
+    dest = path.extname(dest) == ".html" ? dest : path.join(dest, setName + ".html");
+
+    defs = readDir(srcDir);
+    html = libImport + "\n\n<core-iconset-svg id=\"" + setName + "\" iconSize=\"" + size + "\">\n" +
+        "<svg>\n<defs>\n" + defs.join("\n") + "\n</defs>\n</svg>\n</core-icon-set>";
+    fs.writeFileSync(dest, html);
+}
+
+module.exports = generate;
